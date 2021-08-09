78632e7d5805e270da08eaa5261cb7be6eacd2ed
mariamchowdhury.200@gmail.com <mariamchowdhury.200@gmail.com>
2021-08-09 20:58:10 +0600

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TextField, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import DeleteIcon from "@material-ui/icons/Delete";
+import "./App.css";
 
-function App() {
+interface TodoItem {
+  id: number;
+  value: string;
+}
+let count = 1;
+
+export const App: React.FC = () => {
+  const [list, setList] = useState<TodoItem[]>([{ id: 0, value: "" }]);
+
+  const handleChange = (value: string, id: TodoItem["id"]) => {
+    setList((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, value } : item))
+    );
+  };
+
+  const handleDelete = (id: TodoItem["id"]) => {
+    setList((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const handleAdd = (index: number) => {
+    const newItem = { id: count++, value: "" };
+    setList((prev) => [
+      ...prev.slice(0, index + 1),
+      newItem,
+      ...prev.slice(index + 1),
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background font">
+      <div className="todo-container">
+        <h1>To-Do</h1>
+        {list.map((item, index) => (
+          <div key={item.id}>
+            <TextField
+              value={item.value}
+              onChange={(e) => handleChange(e.currentTarget.value, item.id)}
+            />
+            <IconButton onClick={() => handleAdd(index)}>
+              <AddIcon />
+            </IconButton>
+            {list.length > 1 && (
+              <IconButton onClick={() => handleDelete(item.id)}>
+                <DeleteIcon />
+              </IconButton>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
